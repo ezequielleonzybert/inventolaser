a08ee1c22bbc611a56625fc2232401e7c231ffdd
Ezequiel <ezequielleonzybert@gmail.com>
2024-08-17 21:13:45 -0300

    listo

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function loadContent(page, toggleMenu) {
     } else if (page === 'contacto') {
         section = `
         <p>Si tenés alguna consulta o necesitás más información sobre nuestros servicios, no dudes en contactarnos.<br>Estamos ubicados en Haedo, y siempre estamos disponibles para responder a tus preguntas.</p>
-        <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.3147536062788!2d-58.59579578828499!3d-34.63880179662057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc780a6c5da95%3A0x7cdf05dffac6d233!2sLos%20Andes%201400%2C%20B1706BRV%20Haedo%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1723922763444!5m2!1ses-419!2sar" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <p>Teléfno: +54 11 5588-1953</p>
+        <br><iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.3147536062788!2d-58.59579578828499!3d-34.63880179662057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc780a6c5da95%3A0x7cdf05dffac6d233!2sLos%20Andes%201400%2C%20B1706BRV%20Haedo%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1723922763444!5m2!1ses-419!2sar" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <br><p>Teléfono: +54 11 5588-1953</p>
         `;
     }
 
